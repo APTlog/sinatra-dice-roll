d0349c1d67083fe8034ba67aa13cce2c8229ddfb
Paschal Alumona <61502736+APTlog@users.noreply.github.com>
2024-04-27 16:15:26 +0000

    Dice Roll

--- Gemfile
+++ Gemfile
@@ -2,5 +2,7 @@ source "https://rubygems.org"
 
 gem "sinatra"
 gem "puma"
-
+gem "sinatra-contrib"
 gem "rackup", "~> 2.1"
+gem "better_errors"
+gem "binding_of_caller"
--- dice.rb
+++ dice.rb
@@ -1,10 +1,34 @@
+require "better_errors"
+require "binding_of_caller"
 require "sinatra"
 
-get("/howdy") do
-  return "Hello, world!"
-end
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
-get("/") do
-  lucky_num = rand(100)
-  return "Your lucky number for the day is: #{lucky_num} " 
+#get("/howdy") do#
+  return "Hello, world!"
+#end
+
+#get("/") do
+ #lucky_num = rand(100)
+  #return "Your lucky number for the day is: #{lucky_num} " 
+#end
+
+#require "sinatra/reloader"
+#get("/giraffe") do
+ # "Hopefully this shows up without having to restart the server 🤞🏾"
+#end
+
+get("/roll") do
+  first_die = rand(1..6)
+  second_die = rand(1...6)
+  sum = first_die + second_die
+
+  outcome = " You rolled #{first_die} and #{second_die} for a total sum of #{sum}"
+  heading = "<h1>Dice Roll</h1>"
+  link = "<a href= '/roll'> Roll a 2-sided die </a>"
+  # Combine with outcome paragraph
+  content = "#{heading} #{link}<p>#{outcome}</p>"
 end
